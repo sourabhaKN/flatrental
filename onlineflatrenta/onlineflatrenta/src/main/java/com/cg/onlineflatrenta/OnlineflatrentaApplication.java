84042ea18ed4b5e66339caf8f493e951c375a405
Sourabha <sourabha.kn.rao@gmail.com>
2021-05-13 11:20:32 +0530

    Initial commit

---
+++ onlineflatrenta/onlineflatrenta/src/main/java/com/cg/onlineflatrenta/OnlineflatrentaApplication.java
@@ -0,0 +1,13 @@
+package com.cg.onlineflatrenta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineflatrentaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineflatrentaApplication.class, args);
+	}
+
+}
---
+++ onlineflatrenta/onlineflatrenta/src/test/java/com/cg/onlineflatrenta/OnlineflatrentaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.onlineflatrenta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineflatrentaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
